0800707afb800feba6969ae8618260f64740ddc5
Michael Kantor <6068672+kantorcodes@users.noreply.github.com>
2026-03-02 16:51:28 -0500

    feat(discovery): add standards-sdk package path for pkg.go.dev search
    Signed-off-by: Michael Kantor <6068672+kantorcodes@users.noreply.github.com>

---
+++ standards-sdk/doc.go
@@ -0,0 +1,6 @@
+// Package standardssdk is a root-level discovery package for the
+// standards-sdk-go module on pkg.go.dev search.
+//
+// Import path:
+// github.com/hashgraph-online/standards-sdk-go/standards-sdk
+package standardssdk
